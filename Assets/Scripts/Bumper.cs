d9dcd4419cc76c8cb59be50984e6af3dc34d5a57
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-26 23:22:48 -0200

    Adding bumper animation

--- Assets/Scripts/Bumper.cs
+++ Assets/Scripts/Bumper.cs
@@ -6,7 +6,14 @@ public class Bumper : MonoBehaviour {
 
     public float forceOnCollision = 1f;
 
+    Animator animator;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) {
+        animator.SetTrigger("hit");
         Vector2 force = other.transform.position - transform.position;
         force = force.normalized * forceOnCollision;
         other.gameObject.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
