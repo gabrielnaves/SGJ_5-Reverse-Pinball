40cca8b4b597406ff9c0250eec96242dd33e634b
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-26 23:16:47 -0200

    Adding bumpers and ball

---
+++ Assets/Scripts/Bumper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour {
+
+    public float forceOnCollision = 1f;
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Vector2 force = other.transform.position - transform.position;
+        force = force.normalized * forceOnCollision;
+        other.gameObject.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+    }
+}
