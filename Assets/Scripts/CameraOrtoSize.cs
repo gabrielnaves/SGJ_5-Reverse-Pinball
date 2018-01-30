d8451fd3ce7ffc8386fb5f80eddd33ea297c38f4
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-30 20:31:02 -0200

    Adding launchers and configuring some layers

---
+++ Assets/Scripts/CameraOrtoSize.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrtoSize : MonoBehaviour {
+
+    public float pixelsPerUnit = 64f;
+
+    void LateUpdate() {
+        float height = Screen.height;
+        float ortoSize = height / pixelsPerUnit / 2f;
+        Camera.main.orthographicSize = ortoSize;
+    }
+}
