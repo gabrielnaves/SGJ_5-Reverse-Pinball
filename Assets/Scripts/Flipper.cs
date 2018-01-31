420a799585f3a5e68b729c34a5c166c0b2392577
Gabriel Naves da Silva <gabri.navess@gmail.com>
2018-01-30 22:09:18 -0200

    Adding basic AI

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour {
+
+    public bool clockwise;
+    public float rotateAngle = 45f;
+    public float rotateTime = 1f;
+    public bool rotating;
+
+    float elapsedTime;
+    Rigidbody2D rigidbody2d;
+
+    float startAngle;
+    float endAngle;
+
+    void Awake() {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Rotate() {
+        rotating = true;
+        startAngle = 0f;
+        if (!clockwise) endAngle = startAngle + rotateAngle;
+        else endAngle = startAngle - rotateAngle;
+        elapsedTime = 0f;
+    }
+
+    void FixedUpdate() {
+        if (rotating) {
+            elapsedTime += Time.fixedDeltaTime;
+            if (elapsedTime > rotateTime)
+                StartCoroutine(ReturnToStartRotation());
+            else
+                rigidbody2d.MoveRotation(Mathf.Lerp(startAngle, endAngle, elapsedTime / rotateTime));
+        }
+    }
+
+    IEnumerator ReturnToStartRotation() {
+        rotating = false;
+        float elapsedTime = 0f;
+        while (elapsedTime < rotateTime) {
+            rigidbody2d.MoveRotation(Mathf.Lerp(endAngle, startAngle, elapsedTime / rotateTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        rigidbody2d.MoveRotation(startAngle);
+        yield break;
+    }
+}
---
+++ Assets/Scripts/FlipperTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperTrigger : MonoBehaviour {
+
+    public Flipper flipper;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (!flipper.rotating)
+            flipper.Rotate();
+    }
+}
